0e1b63a90743568a5eddead8425d5a97f8a6ac76
Charris <c@harrischris.com>
2019-10-09 08:14:37 +0200

    SSO Auth - WIP Components

--- src/background.ts
+++ src/background.ts
@@ -95,7 +95,7 @@ setupRemoteFunctionsImplementations({
 })
 
 // Attach interesting features onto global window scope for interested users
-// TODO: Shouldn't we prefix these with memex_ to avoid collisions?
+// TODO: prefix these with memex_ to avoid collisions?
 window['authService'] = backgroundModules.auth.authService
 window['authServerFunctions'] = backgroundModules.auth.authServerFunctions
 window['getDb'] = getDb
